e2713ccc9752554c415e258b9789d3ab81ce26c2
valter68 <47627355+valter68@users.noreply.github.com>
2020-03-29 16:07:32 +0200

    Update ChargerPartie.java
    Charge Map sauvegardée il faudra que j'ajoute les autres éléments a save

--- ChargerPartie.java
+++ ChargerPartie.java
@@ -3,8 +3,35 @@
 
 
 package Menu;
+import JeuDeBase.GameState.GamePage;
+import Openning.FileReaderProject;
+import Openning.MatrixMaker;
+import Openning.MyInterface;
+import Openning.Sounds;
+import javax.swing.JOptionPane;
 
 
 public class ChargerPartie {
+    public void saisieMDP() {
+    String name = JOptionPane.showInputDialog("Enter PASSWORD.");
+    
+      String file1 = "SaveMap.txt";
+      FileReaderProject reader1 = new FileReaderProject(file1);
+      String [] row1 = reader1.getinput();
+      System.out.println("loading map");
+      MatrixMaker maker=new MatrixMaker(row1);
+      int [][] matrix = maker.create();
+      
+      Sounds.startmusic();
+      Sounds.loadmusic("themesong");
+      Sounds.play("themesong");
+      
+     
+    MyInterface.window.setPage(new GamePage(0,matrix));
+                
+            
+        }
+    
+
     
 }

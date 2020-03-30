3c819063be376a0f56527f344710095eb483347f
HugJax <31313497+HugJax@users.noreply.github.com>
2020-03-30 15:58:23 +0200

    maj du main

--- SnoopyGame.java
+++ SnoopyGame.java
@@ -1,18 +1,38 @@
 
 package Openning;
 
-import Menu.*;
 import JeuDeBase.GameState.*;
+import JeuDeBase.Mapping.*;
+import JeuDeBase.Entity.*;
+import Menu.*;
 
 
 public class SnoopyGame {
 
     public static void main(String[] args) {
-        Sounds.startmusic();
-        Sounds.loadmusic("theme_song");
-        Sounds.play("theme_song");
-        MyInterface.window.setPage(new GamePage(0));
+      String file1 = "Map.txt";
+      FileReaderProject reader1 = new FileReaderProject(file1);
+      String [] row = reader1.getinput();
+      System.out.println("loading map");
+      MatrixMaker maker=new MatrixMaker(row);
+      int [][] matrix = maker.create();
+      
+      Sounds.startmusic();
+      Sounds.loadmusic("themesong", "sounds/themesong.wav");
+      Sounds.loadmusic("echec", "sounds/echeclevel.wav");
+      Sounds.loadmusic("objet", "sounds/objet.wav");
+      Sounds.loadmusic("succes", "sounds/reussitelevel.wav");
+      Sounds.loadmusic("teleportation", "sounds/teleportation.wav");
+      Sounds.loadmusic("start", "sounds/startlevel.wav");
+      Sounds.play("themesong");
+      
+     
+    MyInterface.window.setPage(new GamePage(0,matrix));
+     //MyInterface.sauvegardeMap();
+    
+        
         
     }
     
 }
+

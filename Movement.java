2419f9d123f640298197b6f93963af0c6b803e12
HugJax <31313497+HugJax@users.noreply.github.com>
2020-03-30 15:02:51 +0200

    Add files via upload
    le dossier entité

---
+++ Movement.java
@@ -0,0 +1,17 @@
+
+package JeuDeBase.Entity;
+
+
+public abstract class Movement {
+    
+    public final static int 
+        UP          = 0,
+        DOWN        = 1,
+        LEFT        = 2,
+        RIGHT       = 3,
+        TOP_LEFT    = 4,
+        TOP_RIGHT   = 5,
+        BOT_LEFT    = 6,
+        BOT_RIGHT   = 7;
+    
+}
